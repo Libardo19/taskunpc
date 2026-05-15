1c230fdf0b7b8cd4967fd12f097a30d882183040
Libardo19 <libardojacosta@unicesar.edu.co>
2026-05-15 01:16:00 -0500

    fix: Creacion de componentes y ddata

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header({ pendientes }) {
+  return (
+    <header>
+      <h1> TaskUPC</h1>
+      <p> Tienes {pendientes} tareas pendientes </p>
+    </header>
+  );
+}
+export default Header;
---
+++ src/data/tareas.js
@@ -0,0 +1,37 @@
+export const tareasIniciales = [
+  {
+    id: 1,
+    titulo: "Parcial de Cálculo",
+    materia: "Matemáticas",
+    fecha: "2026-05-10",
+    completada: false,
+  },
+  {
+    id: 2,
+    titulo: "Informe de Laboratorio",
+    materia: "Física",
+    fecha: "2026-05-08",
+    completada: true,
+  },
+  {
+    id: 3,
+    titulo: "Parcial de programacion",
+    materia: "Programacion",
+    fecha: "2026-05-09",
+    completadas: true,
+  },
+  {
+    id: 4,
+    titulo: "Quiz de Arquitectuta",
+    materia: "Diseño de software",
+    fecha: "2026-05-14",
+    completadas: false,
+  },
+  {
+    id: 5,
+    titulo: "Proyecto de aula",
+    materia: "Programacion",
+    fecha: "2026-05-18",
+    completadas: false,
+  },
+];
